97bec48b75cb00743bcc820a144e5c1a9dbc4df4
Maya Babuji <mayababuji@outlook.com>
2026-06-15 10:34:31 -0400

    remove main method in homepage for demo

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -31,7 +31,5 @@ public class HomePage {
 //        driver.findElement(searchBox).sendKeys(foodCategory);
 //        wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
 //    }
-public static void main(String[] args) {
-    
-}
+
 }
